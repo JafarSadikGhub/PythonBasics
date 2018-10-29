4ba05765a38667e049bec9df31c1ad97b54fdfa9
jafar95 <jafar,sadik@northsouth.edu>
2018-10-29 23:40:21 +0600

    Interesting FYI

---
+++ PointNineIsNot1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 29 23:24:44 2018
+
+@author: Ja4rsPC
+"""
+
+x = 0;
+for i in range(10):
+    x = x + 0.1
+if x == 1:
+    print( x, ' is = 1.0')
+else:
+    print( x, ' is not 1.0')    
+    
